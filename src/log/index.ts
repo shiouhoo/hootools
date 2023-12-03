bc91ff02daff35d09ab172e1448cdd6a36e025e4
shiouhoo <shiouhoo@foxmail.com>
2023-12-03 18:18:24 +0800

    feat: 完成log

--- src/log/index.ts
+++ src/log/index.ts
@@ -2,28 +2,34 @@ import * as vscode from 'vscode';
 
 // 所有语言都执行
 // 只在用户键入触发字符（例如点或冒号）或手动触发自动完成（例如通过按下 Ctrl+Space）时被调用
-export const logDisposable = vscode.languages.registerCompletionItemProvider('*',
+export const logDisposable = vscode.languages.registerCompletionItemProvider(
+    '*',
     {
         provideCompletionItems(document, position) {
             const linePrefix = document.lineAt(position).text;
-            if (!linePrefix.endsWith('.log') && !linePrefix.endsWith('.lo') && !linePrefix.endsWith('.l') && !linePrefix.endsWith('.')) {
+            // 获取..前面的单词
+            const wordPattern = /[\w.]+/; // Matches words that may include dots
+            const wordRange = document.getWordRangeAtPosition(position, wordPattern);
+            const word = document.getText(wordRange).slice(0, -2);
+            if(!wordRange || !word) {
                 return undefined;
             }
-            console.log(document.lineAt(position).text);
-
-            // 获取点前面的单词
-            const wordRange = document.getWordRangeAtPosition(new vscode.Position(position.line, position.character - 4));
-            if (!wordRange) {
+            // 不是以..log结尾的，不触发
+            if (!linePrefix.endsWith('..log') && !linePrefix.endsWith('..lo') && !linePrefix.endsWith('..l') && !linePrefix.endsWith('..')) {
                 return undefined;
             }
-            const word = document.getText(wordRange);
 
-            const completionItem = new vscode.CompletionItem('.log', vscode.CompletionItemKind.Snippet);
-            completionItem.insertText = new vscode.SnippetString(`console.log(\`${word}: ${word}\`)`);
+            const completionItem = new vscode.CompletionItem('log', vscode.CompletionItemKind.Snippet);
+            completionItem.insertText = new vscode.SnippetString(`console.log(\`${word}: ${'\\${' + word + '}'}\`);`);
+            completionItem.documentation = new vscode.MarkdownString(`console.log(\`${word}: \${${word}};\`)`);
+            completionItem.detail = 'hootools：快捷输出';
+            completionItem.tags = [vscode.CompletionItemTag.Deprecated];
 
-            // 删除点前面的单词
-            completionItem.documentation = new vscode.MarkdownString(`hootools：快捷输出 console.log(\`${word}: ${word};\`)`);
+            const start = new vscode.Position(position.line, position.character - word.length - 2);
+            const end = position;
+            const range = new vscode.Range(start, end);
 
+            completionItem.additionalTextEdits = [vscode.TextEdit.delete(range)];
             return [completionItem];
         },
     },
